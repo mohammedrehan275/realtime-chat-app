9eceb848e9f2571a4319243b6bf2859520f86369
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2021-10-01 15:01:20 +0530

    connecting client and server with socket.io

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-function Chat() {
+import './Chat.css';
+
+let socket;
+
+function Chat({location}) {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT)
+
+        setName(name)
+        setRoom(room)
+
+        socket.emit('join', {name, room}, () => {
+
+        })
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+
+    }, [location.search, ENDPOINT])
     return (
         <div>
             Chat
