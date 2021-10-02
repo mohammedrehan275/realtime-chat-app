46517c65e101733f7d85604f1472b3d3d3cac013
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2021-10-02 15:39:11 +0530

    added send message functionality using socket io

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,12 +3,16 @@ import queryString from 'query-string'
 import io from 'socket.io-client'
 
 import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 let socket;
 
 function Chat({location}) {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
@@ -30,9 +34,29 @@ function Chat({location}) {
         }
 
     }, [location.search, ENDPOINT])
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message])
+        })
+    }, [messages])
+
+    const sendMessage = (event) => {
+        event.preventDefault()
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+    console.log(message, messages)
+
     return (
-        <div>
-            Chat
+        <div className='outerContainer'>
+            <div className='container'>
+                <InfoBar room={room}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+            </div>
         </div>
     )
 }
